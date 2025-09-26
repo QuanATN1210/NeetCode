9abc1e0b9253e0e65d38a4c90faa46c190b9651c
QuanATN1210 <nguyentrananhquan121005@gmail.com>
2025-09-26 11:41:12 +0700

    .

--- Two pointer/Trapping rain water.py
+++ Two pointer/Trapping rain water.py
@@ -28,5 +28,29 @@ class Solution:
             res += min(prefix[i],suffix[i]) - height[i]
         return res
 '''
+Time complexity O(n)
+Space complexity O(n)
+'''
 
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) == 0:
+            return 0
+        l,r = 0, len(height) -1
+        n = len(height) -1
+        res = 0
+        leftMax,rightMax = height[0], height[n]
+        while l < r:
+            if leftMax < rightMax :
+                l+=1
+                leftMax = max(leftMax,height[l])
+                res += leftMax - height[l]
+            else:
+                r-=1
+                rightMax = max(rightMax,height[r])
+                res+= rightMax - height[r]
+        return res
+'''
+Time complexity O(n)
+Space complexity O(1)
 '''
