289d8bae7d47d2c195a5b16b47fccacf9f3a51ef
QuanATN1210 <nguyentrananhquan121005@gmail.com>
2025-10-05 02:22:21 +0700

    .

---
+++ Linked List/Reverse linked list.py
@@ -0,0 +1,33 @@
+'''
+Reverse Linked List
+Solved 
+Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+'''
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr, prev = head, None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        return prev
+
+'''
+Time complexity O(n)
+Space complexity O(1)
+'''
